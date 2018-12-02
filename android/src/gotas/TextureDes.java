e7bfa476ecf2b422c00abf5fd229bd552279aba9
Nicolas <nmahechab@gmail.com>
2018-12-02 13:31:40 -0500

    Update division game

---
+++ android/src/gotas/TextureDes.java
@@ -0,0 +1,9 @@
+package gotas;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class TextureDes extends Texture {
+    public TextureDes(String internalPath, String descripcion){
+        super(internalPath);
+    }
+}
